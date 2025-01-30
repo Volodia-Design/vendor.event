8e913d5de9653ca7e1279bbe17ce706320761ae4
Gishyan888 <artgish1964@gmail.com>
2025-01-31 02:53:43 +0400

    about logout

--- src/Layouts/Sidebar.jsx
+++ src/Layouts/Sidebar.jsx
@@ -2,10 +2,13 @@ import { useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import sidebarItems from "../utils/sidebarItems";
 import useActiveTab from "../store/useActiveTab";
+import api from "../utils/api";
+import useLoading from "../store/useLoading";
 
 export default function Sidebar() {
   const location = useLocation();
   const { activeTab, setActiveTab } = useActiveTab();
+  const { setIsLoading } = useLoading();
 
   useEffect(() => {
     const currentTab = sidebarItems.find((item) => {
@@ -29,9 +32,23 @@ export default function Sidebar() {
   }, [location.pathname, setActiveTab]);
 
   const handleGoMainPage = () => {
-    window.location.href = "https://eml-gishyans-projects.vercel.app/";
+    window.open("https://eml-gishyans-projects.vercel.app/", "_blank");
   };
-
+  
+const handleLogout = () => {
+setIsLoading(true);
+  api.post("/auth/sign-out")
+    .then(() => {
+      localStorage.removeItem("authToken");
+      window.location.href = "https://eml-gishyans-projects.vercel.app/";
+    })
+    .catch((error) => {
+      console.error("Error logging out:", error);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+}
   return (
     <div className="hidden lg:block p-4 h-full w-full max-w-80">
       <div
@@ -70,6 +87,19 @@ export default function Sidebar() {
         </div>
 
         <div className="mt-auto">
+          <div
+            onClick={() => handleLogout()}
+            className="flex justify-center items-center gap-3 p-4 cursor-pointer"
+          >
+            <div className="flex items-center gap-1">
+              <img
+                src="/Images/SidebarIcons/Logout.svg"
+                alt="Logo"
+                className="h-6"
+              />
+              <p className="text-text3 text-white">Log out</p>
+            </div>
+          </div>
           <div
             onClick={() => handleGoMainPage()}
             className="flex justify-center items-center gap-3 p-4 rounded-2xl bg-[#192634] mr-4 hover:bg-black-100/40  duration-300 ease-in-out cursor-pointer"
--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import Button from "../../components/Button";
+import api from "../../utils/api";
+import useLoading from "../../store/useLoading";
+import useModal from "../../store/useModal";
+
 export default function AboutUs() {
+  const [aboutData, setAboutData] = useState("");
+  const { setIsLoading } = useLoading();
+  const {
+    showSuccess,
+    showError,
+  } = useModal();
+
+  useEffect(() => {
+    getVendorAbout();
+  }, []);
+
+  const getVendorAbout = () => {
+    setIsLoading(true);
+    api
+      .get("/vendor-about")
+      .then((res) => {
+        setAboutData(res.data.data.text);
+      })
+      .catch((err) => {
+        console.error("Error fetching vendor about data:", err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  };
+
+  const saveData = () => {
+    setIsLoading(true);
+    api
+      .put("/vendor-about", { text: aboutData })
+      .then((res) => {
+        showSuccess();
+      })
+      .catch((err) => {
+        showError();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  };
+
   return (
-    <div>AboutUs</div>
-  )
+    <div className="w-full flex flex-col items-start gap-3 bg-white p-3 rounded-2xl lg:px-6 px-2">
+      <textarea
+        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-1 focus:ring-secondary-500"
+        placeholder="Short information about the Vendor"
+        value={aboutData}
+        onChange={(e) => setAboutData(e.target.value)}
+        rows={4}
+      />
+      <div className="w-full flex gap-3 items-center justify-end mt-2">
+        <Button
+          text={"Save"}
+          onClick={saveData}
+          buttonStyles="bg-secondary-800 hover:bg-secondary-700 text-white py-2 px-6"
+        />
+      </div>
+    </div>
+  );
 }
